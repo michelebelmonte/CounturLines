98ac81197d0ef5e9623905d155d83dae3996546d
michelebelmonte <michele.belmonte@gmail.com>
2016-04-26 18:39:16 +0100

    First result
    First result.
    Next steps:
    1. move computation in a module
    2. add unit tests, instead of print statements

--- testValidities.ml
+++ testValidities.ml
@@ -158,5 +158,5 @@ let computeIsoSegments ~quota ~left ~top  ~right ~bottom ~leftTopValue ~rightTop
   getIsoSegments quota quad
 							     
 let () =
-  let point={x=1.0;y=2.0;z=3.0} in
-  Printf.printf "hello! %f\r\n" point.x
+  let segments =computeIsoSegments ~quota:1.0 ~left:0.0 ~top:1.0  ~right:1.0 ~bottom:0.0 ~leftTopValue:2.0 ~rightTopValue:0.0 ~leftBottomValue:0.0 ~rightBottomValue:0.0 in
+  List.iter  (fun x -> Printf.printf "v0.x=%f, v0.y=%f, v1.x=%f, v1.y=%f\r\n" x.v0.x x.v0.y x.v1.x x.v1.y) segments
